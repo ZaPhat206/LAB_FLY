1aacf8e77c729378bc86def407ac5dae23b7c73b
ZaPhat206 <giaphat16032006@gmail.com>
2026-08-01 23:26:13 +0700

    refactor: update tqdm progress bar settings to improve update frequency and visibility

--- utils.py
+++ utils.py
@@ -24,7 +24,7 @@ def get_parameters(model: nn.Module):
 def feature_extract(model: nn.Module, data_loader: DataLoader, device: torch.device):
     embedding_list, label_list = [], []
     with torch.no_grad():
-        for i, (data, label) in enumerate(tqdm(data_loader, leave=False, dynamic_ncols=True, mininterval=1.0)):
+        for i, (data, label) in enumerate(tqdm(data_loader, leave=True, dynamic_ncols=True, mininterval=0.5)):
             data, label = data.to(device), label.to(device)
             embedding = model(data)
             embedding_list.append(embedding)
